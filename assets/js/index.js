ecb05f11846002842ff2c384292693216bc7078a
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-16 20:03:33 +0530

    Improved UI, added page load animation and added custom fonts

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+
+gsap.to(".loader", {
+    duration: 2,
+    height: "0",
+    top: "100%",
+    ease: "Expo.easeInOut",
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+function scrollToId(event, id, scrollAmount) {
+    event.preventDefault();
+    const element = document.getElementById(id);
+    const offset = element.getBoundingClientRect().top + window.scrollY;
+    const targetScroll = offset - scrollAmount;
+    window.scrollTo({
+        top: targetScroll,
+        behavior: "smooth",
+    });
+}
+
